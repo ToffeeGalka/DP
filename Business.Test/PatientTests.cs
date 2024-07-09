2fb3c515193cd48d509119cb6c0c2c015ca6c4b2
ToffeeGalka <157503739+ToffeeGalka@users.noreply.github.com>
2024-07-09 16:35:49 +0300

    Homework32 (#2)

---
+++ Business.Test/PatientTests.cs
@@ -0,0 +1,183 @@
+using Business.Models;
+using Business.Mappers;
+using Business.Services;
+using WebData.Entities;
+using Moq;
+using WebData.Enums;
+using System.Net;
+using System.Numerics;
+using Business.Validators;
+
+
+
+namespace Business.Test
+{
+    public class PatientTests
+    { 
+        private Patient _patient;
+        private Mock<IPatientMapper> _mapperMock;
+        private IPatientService _underTest;
+        private Mock<WebData.AppDbContext> _appDbContextMock;       
+ 
+        [SetUp]
+        public void Setup()
+        {
+            _mapperMock = new Mock<IPatientMapper>();
+            _appDbContextMock = new Mock<WebData.AppDbContext>();
+            _underTest = new PatientService(_appDbContextMock.Object, _mapperMock.Object);         
+        }
+
+        [Test]
+        public void AddPatient_ValidateGenderIsInvalid()
+        {
+            //Arrange
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ò",
+                Address = "",
+                Phone = ""
+            };
+
+            //Act
+             var act = _underTest.AddPatient(_patient);
+            //Assert
+             Assert.ThrowsAsync<Exception>(() => act);
+        }
+
+        [Test]
+        public void AddPatient_ValidateSurNameIsNullOrEmpty()
+        {
+            //Arrange
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "",
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = "",
+                Phone = ""
+            };
+            //Act
+            var act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = null,
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = "",
+                Phone = ""
+            };
+            //Act
+            act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+        }
+        [Test]
+        public void AddPatient_ValidateNameIsNullOrEmpty()
+        {
+            //Arrange
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = "",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = "",
+                Phone = ""
+            };
+            //Act
+            var act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = null,
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = "",
+                Phone = ""
+            };
+            //Act
+            act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+        }
+        [Test]
+        public void AddPatient_ValidateAddressIsNullOrEmpty()
+        {
+            //Arrange
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = "",
+                Phone = ""
+            };
+            //Act
+            var act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today),
+                Sex = "ì",
+                Address = null,
+                Phone = ""
+            };
+            //Act
+            act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+        }
+
+        [Test]
+        public void AddPatient_ValidateDateOfBirthIsInvalid()
+        {
+            //Arrange
+            _patient = new Patient()
+            {
+                Id = 0,
+                SurName = "111",
+                Name = "111",
+                SecName = "",
+                DateOfBirth = DateOnly.FromDateTime(DateTime.Today).AddDays(1),
+                Sex = "ì",
+                Address = "111",
+                Phone = ""
+            };
+
+            //Act
+            var act = _underTest.AddPatient(_patient);
+            //Assert
+            Assert.ThrowsAsync<Exception>(() => act);
+        }
+    }
+}
